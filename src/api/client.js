4c07b0d63edf326a016f5e286ae65a1d2a48b9df
nicolahadwen <nicola@ritual.co>
2019-12-31 23:18:12 -0500

    got local working

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const client = axios.create({
-  baseURL: 'localhost:8080/v1/',
+  baseURL: 'http://localhost:8080/v1/',
   timeout: 4000,
   headers: {'X-Custom-Header': 'foobar'}
 });

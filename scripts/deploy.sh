7a92e45966c2910ab9d1a27c54a8e018f44be6c2
nicolahadwen <nicola@ritual.co>
2020-02-01 15:44:58 -0500

    done

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+docker build -t hadwen/web-ui:0.0.00 .
 docker push hadwen/web-ui:0.0.00
 kubectl apply -f ./deployment.yaml
--- server.js
+++ server.js
@@ -6,8 +6,20 @@ require('dotenv').config();
 
 const app = express();
 
+const createHttpConfig = () => {
+  try {
+    return {
+      key: fs.readFileSync('/etc/https-certs/key.pem'),
+      cert: fs.readFileSync('/etc/https-certs/cert.pem')
+    };
+  } catch (e) {
+    console.info('the error: ', e);
+  }
+};
+
 app.use(express.static('dist'));
 app.use('/', (req, res) => {
+  console.log('sending file');
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
@@ -15,11 +27,8 @@ if(process.env.NODE_ENV === 'development') {
   app.listen(process.env.PORT);
   console.log(`Listening on ${process.env.PORT}`);
 } else {
-  https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-  }, app)
-  .listen(process.env.PORT, function () {
-    console.log(`Listening on ${process.env.PORT}`)
-  });
+  console.log('httpsOptions: ', createHttpConfig());
+  console.info('HTTPS RUN: ', process.env.PORT);
+  https.createServer(createHttpConfig(), app).listen(process.env.PORT);
 }
+
